abe16339b699425517f29294d315eca42466cad7
amirhr21 <amirhossein.rafiee.80@gmail.com>
2022-03-09 17:29:22 +0330

    revert

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddelware'
 ]
 
 ROOT_URLCONF = 'mysite.urls'
@@ -128,9 +132,15 @@ STATICFILES_DIRS = [
     BASE_DIR / "statics",
 ]
 
+<<<<<<< HEAD
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+=======
+STATICFILES_STIRAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+>>>>>>> parent of 012ef2c (Revert "tweaked app for heroku")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
